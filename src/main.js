ad2bb619ca57d61b6fd725fa2d02f503b1ed943f
ndrslmpk <andreas.lempik@gmail.com>
2022-05-07 18:19:27 +0200

    Vue-JS | added more data, implemented toggleDarkmode(), added static addTodo()

--- src/main.js
+++ src/main.js
@@ -4,8 +4,47 @@ import { createApp } from 'vue/dist/vue.esm-bundler';
 const app = createApp({
   data() {
     return {
-      description: 'Never stop learning'
+      description: 'Never stop learning',
+      inputtype: "text",
+      darkmode: true,
+      github: {
+        name: "Ndrslmpk",
+        link: "http://google.de",
+      },
+      todos:[{
+        description: "I have to learn Vue",
+        done: false,
+      },
+      {
+        description: "I teach myself every day",
+        done: true,
+      }]
     }
+  },
+  methods: {
+    toggleDarkmode(){
+      this.darkmode = !this.darkmode
+    },
+    addTodo(){
+      console.log(document.getElementById('todolist'));
+      const el = document.getElementById('todolist');
+
+      var newitemLi = document.createElement('li');
+
+      var newitemLiInput = document.createElement('input');
+      newitemLiInput.type = 'checkbox';
+      newitemLiInput.name = 'todo';
+      newitemLiInput.checked = false;
+
+      var newitemLiSpan = document.createElement('span');
+      newitemLiSpan.textContent = 'newTodo';
+
+      newitemLi.appendChild(newitemLiInput);
+      newitemLi.appendChild(newitemLiSpan);
+
+      el.appendChild(newitemLi);
+
+    },
   }
 });
 
